c3517bf3700daa9c18d37091d24bd80f24dbbbff
git4qa <9wendigo@gmail.com>
2021-04-07 17:01:26 +0300

    Выполнил задание урока 6 модуля 4 (https://stepik.org/lesson/199980/step/6?unit=174035)

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose interface language (default=en)')
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    print("\nStart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
